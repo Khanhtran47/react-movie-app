3745ea6f1fd3e1a4c5b21c0abee98585c9bdb2d1
Khanhtran47 <tranduckhanh23@gmail.com>
2021-10-19 02:13:13 +0200

    creating footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,42 @@
+import "./footer.scss"
+
+import { Link } from "react-router-dom"
+
+import bg from "../../assets/footer-bg.jpg"
+import logo from "../../assets/tmovie.png"
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img src={logo} alt="" />
+            <Link to="/">tMovies</Link>
+          </div>
+        </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Privacy Policy</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Must Watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
