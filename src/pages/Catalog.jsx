3c5f20a09c5ca465ff8af96645f75dad2b80a57c
Khanhtran47 <tranduckhanh23@gmail.com>
2021-10-19 02:29:04 +0200

    creating catalog page and page header

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,5 +1,19 @@
+import { useParams } from "react-router"
+
+import PageHeader from "../components/page-header/PageHeader"
+
+import { category as cate } from "../api/tmdbApi"
+
 const Catalog = () => {
-  return <div>Catalog</div>
+  const { category } = useParams()
+
+  return (
+    <>
+      <PageHeader>
+        {category === cate.movie ? "Movies" : "TV Series"}
+      </PageHeader>
+    </>
+  )
 }
 
 export default Catalog
