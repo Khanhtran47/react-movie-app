af45b5a9d37cf50698e19ea6e7985b5570f7c6e7
Khanhtran47 <tranduckhanh23@gmail.com>
2021-10-20 01:42:38 +0200

    creating movies and tv series grid layout

---
+++ src/components/movie-grid/MovieGrid.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router"
+
+import "./movie-grid.scss"
+
+import MovieCard from "../movie-card/MovieCard"
+import { OutlineButton } from "../button/Button"
+
+import tmdbApi, { category, movieType, tvType } from "../../api/tmdbApi"
+
+const MovieGrid = (props) => {
+  const [items, setItems] = useState([])
+
+  const [page, setPage] = useState(1)
+  const [totalPage, setTotalPage] = useState(0)
+
+  const { keyword } = useParams()
+
+  useEffect(() => {
+    const getList = async () => {
+      let response = null
+
+      if (keyword === undefined) {
+        const params = {}
+        switch (props.category) {
+          case category.movie:
+            response = await tmdbApi.getMoviesList(movieType.upcoming, {
+              params,
+            })
+            break
+          default:
+            response = await tmdbApi.getTvList(tvType.popular, { params })
+        }
+      } else {
+        const params = {
+          query: keyword,
+        }
+        response = await tmdbApi.search(props.category, { params })
+      }
+      setItems(response.results)
+      setTotalPage(response.total_pages)
+    }
+    getList()
+  }, [props.category, keyword])
+
+  const loadMore = async () => {
+    let response = null
+
+    if (keyword === undefined) {
+      const params = {
+        page: page + 1,
+      }
+      switch (props.category) {
+        case category.movie:
+          response = await tmdbApi.getMoviesList(movieType.upcoming, {
+            params,
+          })
+          break
+        default:
+          response = await tmdbApi.getTvList(tvType.popular, { params })
+      }
+    } else {
+      const params = {
+        page: page + 1,
+        query: keyword,
+      }
+      response = await tmdbApi.search(props.category, { params })
+    }
+    setItems([...items, ...response.results])
+    setPage(page + 1)
+  }
+
+  return (
+    <>
+      <div className="movie-grid">
+        {items.map((item, i) => (
+          <MovieCard category={props.category} item={item} key={i} />
+        ))}
+      </div>
+      {page < totalPage ? (
+        <div className="movie-grid__loadmore">
+          <OutlineButton className="small" onClick={loadMore}>
+            Load more
+          </OutlineButton>
+        </div>
+      ) : null}
+    </>
+  )
+}
+
+export default MovieGrid
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router"
 
 import PageHeader from "../components/page-header/PageHeader"
+import MovieGrid from "../components/movie-grid/MovieGrid"
 
 import { category as cate } from "../api/tmdbApi"
 
@@ -12,6 +13,11 @@ const Catalog = () => {
       <PageHeader>
         {category === cate.movie ? "Movies" : "TV Series"}
       </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category} />
+        </div>
+      </div>
     </>
   )
 }
