b516ebacf94c6002372f726a7dc2302af2c101b8
Khanhtran47 <tranduckhanh23@gmail.com>
2021-10-20 02:58:31 +0200

    creating detail page

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -2,7 +2,7 @@ import { Route, Switch } from "react-router-dom"
 
 import Home from "../pages/Home"
 import Catalog from "../pages/Catalog"
-import Detail from "../pages/Detail"
+import Detail from "../pages/detail/Detail"
 
 import React from "react"
 
